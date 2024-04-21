f57a877b0d6f638db3bc69c2758c9ac9bdcf3e3a
Riya Patel <rpatel05@terpmail.umd.edu>
2024-04-21 13:02:10 -0400

    Started creating the while loop with its conditionals for guessing letters

--- consolidation_project_1.py
+++ consolidation_project_1.py
@@ -3,4 +3,27 @@
 import os
 import random
 
+# Chooses a word from the word bank
+def choose_word():
+    word_bank = ["christmas", "thanksgiving", "halloween", "easter", "kwanzaa"]
+    return random.choice(word_bank)
+
+# Welcomes player into the game
 print("Welcome to the Word Guessing Game!")
+
+word = choose_word()
+letters_guessed = []
+words_guessed = []
+letter_counter = 0
+word_counter = 0
+
+# The while loop that processes the entire game
+turns = 0
+while turns in range (3):
+    guess = input("Guess a letter or guess the secret word: ")
+    if len(guess) == 1:
+        if guess in word:
+            letters_guessed.append(guess)
+            count = word.count(guess)
+            if count == 1:
+                print(f"The letter {guess} appears once in the word.")
